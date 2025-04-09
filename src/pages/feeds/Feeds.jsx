69e16d8ec9b75691b8c26e77fc381dc69bf94bc9
Imranhasanrimon <imranhasanrimon5@gmail.com>
2025-04-09 11:53:23 +0600

    adding student profile

--- src/pages/feeds/Feeds.jsx
+++ src/pages/feeds/Feeds.jsx
@@ -2,12 +2,13 @@ import useAxiosSecure from "@/hooks/useAxiosSecure";
 import PostModal from "./PostModal";
 import { useQuery } from "@tanstack/react-query";
 import PostCard from "./PostCard";
+import LoadingSpinner from "@/myComponents/LoadingSpinner";
 
 
 
 const Feeds = () => {
     const axiosSecure = useAxiosSecure();
-    const { data: posts = [], refetch } = useQuery({
+    const { data: posts = [], refetch, isLoading } = useQuery({
         queryKey: ["posts"],
         queryFn: async () => {
             const res = await axiosSecure("/posts/allPosts");
@@ -15,13 +16,28 @@ const Feeds = () => {
         }
     })
 
+    if (isLoading) return <LoadingSpinner />
     return (
-        <div className="p-4">
-            <PostModal refetch={refetch} />
-            <div className="mt-4 grid lg:grid-cols-2 max-w-5xl mx-auto gap-4">
-                {posts.map(post => <PostCard key={post._id} post={post}></PostCard>)}
-            </div>
+        <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr_250px] min-h-screen p-4 max-w-7xl mx-auto">
+            {/* Left Sidebar */}
+            <aside className="hidden lg:block sticky top-18 h-screen overflow-y-auto border-r px-4">
+                <div className="text-xl font-bold mb-4">Left Sidebar</div>
+                {/* Add your sidebar content here */}
+            </aside>
 
+            {/* Main Content */}
+            <main >
+                <PostModal refetch={refetch} />
+                <div className="mt-4 grid  mx-auto gap-4">
+                    {posts.map(post => <PostCard key={post._id} post={post}></PostCard>)}
+                </div>
+            </main>
+
+            {/* Right Sidebar */}
+            <aside className="hidden lg:block sticky top-18 h-screen overflow-y-auto border-l px-4 ">
+                <div className="text-xl font-bold mb-4">Right Sidebar</div>
+                {/* Add your sidebar content here */}
+            </aside>
         </div>
     );
 };
--- src/pages/feeds/PostCard.jsx
+++ src/pages/feeds/PostCard.jsx
@@ -1,13 +1,36 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import { Angry, EllipsisVertical, Frown, Heart, ThumbsUp } from "lucide-react";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 
 const PostCard = ({ post }) => {
     const { postBody, studentId, userInfo } = post;
 
     return (
-        <Card className="w-full max-w-xl mx-auto shadow-md">
-            <CardHeader className="flex flex-row items-center space-x-2 ">
+        <Card className="w-full max-w-xl mx-auto shadow-md p-4">
+            <div className="flex flex-row items-center gap-4 relative">
                 <Avatar>
                     <AvatarImage src={userInfo?.image} alt="User Image" />
                     <AvatarFallback>{userInfo?.name?.charAt(0)}</AvatarFallback>
@@ -18,13 +41,99 @@ const PostCard = ({ post }) => {
                     </span></CardTitle>
                     <p className="text-sm text-muted-foreground">ID: {studentId}</p>
                 </div>
-            </CardHeader>
+
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="outline" className="absolute right-0 w-8 h-8 cursor-pointer"><EllipsisVertical /></Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-56">
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuGroup>
+                            <DropdownMenuItem>
+                                Profile
+                                <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                Billing
+                                <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                Settings
+                                <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                Keyboard shortcuts
+                                <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+                            </DropdownMenuItem>
+                        </DropdownMenuGroup>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuGroup>
+                            <DropdownMenuItem>Team</DropdownMenuItem>
+                            <DropdownMenuSub>
+                                <DropdownMenuSubTrigger>Report Post</DropdownMenuSubTrigger>
+                                <DropdownMenuPortal>
+                                    <DropdownMenuSubContent>
+                                        <DropdownMenuItem>Biased</DropdownMenuItem>
+                                        <DropdownMenuItem>Bullying</DropdownMenuItem>
+                                        <DropdownMenuItem>Misinformation </DropdownMenuItem>
+                                        <DropdownMenuItem>Scam </DropdownMenuItem>
+                                        <DropdownMenuItem>sexually explicit</DropdownMenuItem>
+                                    </DropdownMenuSubContent>
+                                </DropdownMenuPortal>
+                            </DropdownMenuSub>
+                            <DropdownMenuItem>
+                                New Team
+                                <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+                            </DropdownMenuItem>
+                        </DropdownMenuGroup>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>GitHub</DropdownMenuItem>
+                        <DropdownMenuItem>Support</DropdownMenuItem>
+                        <DropdownMenuItem disabled>API</DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>
+                            Log out
+                            <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
 
             <Separator />
 
-            <CardContent className="pt-4">
+            <CardContent className="p-0">
                 <p className="whitespace-pre-line">{postBody}</p>
                 {post?.imageURL && <img className="rounded-lg mt-3" src={post?.imageURL} alt="post photo" />}
+
+                <div className="mt-3 flex items-center gap-4">
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger><Button className="cursor-pointer text-red-500" variant="outline"><Heart /></Button></TooltipTrigger>
+                            <TooltipContent>
+                                <p>Love</p>
+                            </TooltipContent>
+                        </Tooltip>
+                        <Tooltip>
+                            <TooltipTrigger><Button className="cursor-pointer text-yellow-500" variant="outline"><Frown /></Button></TooltipTrigger>
+                            <TooltipContent>
+                                <p>Sad</p>
+                            </TooltipContent>
+                        </Tooltip>
+                        <Tooltip>
+                            <TooltipTrigger><Button className="cursor-pointer text-sky-500" variant="outline"><ThumbsUp /></Button></TooltipTrigger>
+                            <TooltipContent>
+                                <p>Like</p>
+                            </TooltipContent>
+                        </Tooltip>
+                        <Tooltip>
+                            <TooltipTrigger><Button className="cursor-pointer text-red-500" variant="outline"><Angry /></Button></TooltipTrigger>
+                            <TooltipContent>
+                                <p>Angry</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                </div>
 
             </CardContent>
         </Card>
